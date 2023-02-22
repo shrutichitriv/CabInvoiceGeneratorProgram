ce0d379649575a413c443713f27368e8f345e3de
shrutichitriv <shrutichitriv199620@gmail.com>
2023-02-22 17:07:55 +0530

    [Shruti] Perform Cab invoice generator program

---
+++ CabInvoiceGenerator_244/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace CabInvoiceGenerator_244
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Cab Invoice Generator Program");
+ 
+        }
+    }
+}
